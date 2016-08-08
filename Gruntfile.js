419e1fd28659380b8c0904aae52d66d6f384d33f
Nutan Mungila <nutanmungila@gmail.com>
2016-08-08 11:53:43 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    watch:{
+      sass:{
+        files:['sass/*.scss'],
+        tasks:['sass']
+      }
+    },
+
+    sass: {
+
+        dist: {
+            files: {
+                'css/style.css' : 'sass/style.scss'
+            }
+        }
+    }
+ });
+
+  // Load the plugin that provides the "uglify" task.
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass','watch']);
+
+};
+//'main.css': 'main.scss'
